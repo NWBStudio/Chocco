595507185eef3e3e535435671c6e3794a08c68d5
NWB Admin <paradoxum912@gmail.com>
2019-12-16 00:20:45 +0300

    Сделал JS аккордеона меню

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
+/////////////////////// полноэкранное меню
+
 const burgerMenu = document.querySelector('.header__burger-menu');
-const burgerMenuComputed = getComputedStyle(burgerMenu);
 const fnav = document.querySelector('.fullscreen-nav');
 const fnavCloseBtn = document.querySelector('.fullscreen-nav__close-btn');
 function noScroll() {
@@ -15,3 +16,59 @@ fnavCloseBtn.addEventListener('click', () => {
     fnav.style.display = 'none';
     window.removeEventListener('scroll', noScroll);
 });
+
+
+////////////////////// секция меню аккордеон
+
+const   menu = document.querySelector('.menu'),
+        collapsible = document.querySelectorAll('.accordeon-menu__item-collapsible'),
+        collapsibleLength = collapsible.length,
+        collapsibleInfo = document.querySelectorAll('.accordeon-menu__info'),
+        menuCloseBtn = document.querySelectorAll('.accordeon-menu__close-btn');
+
+menu.addEventListener('click', (e) => {
+    for (let i = 0; i < collapsibleLength; i++) {
+        collapsible[i].classList.remove('accordeon-menu__item-collapsible--active');
+        
+    }
+});        
+
+for (let i = 0; i < collapsibleLength; i++) {
+    collapsible[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        menuCloseBtn.forEach(element => { //реализация закрытия через крестик
+            element.addEventListener('click', (e) => {
+                collapsible[i].classList.remove('accordeon-menu__item-collapsible--active');
+                for (let i = 0; i < collapsibleLength; i++) {
+                    collapsible[i].style.display = 'flex';
+                }
+            });
+        });
+
+        if (collapsible[i].classList.contains('accordeon-menu__item-collapsible--active')){
+            collapsible[i].classList.remove('accordeon-menu__item-collapsible--active');
+            
+        } else {
+            for (let i = 0; i < collapsibleLength; i++) {
+                if (collapsible[i].classList.contains('accordeon-menu__item-collapsible--active')){
+                    collapsible[i].classList.remove('accordeon-menu__item-collapsible--active');
+                }
+            }
+            collapsible[i].classList.add('accordeon-menu__item-collapsible--active');
+            if (window.matchMedia("(max-width: 480px)").matches) {
+                for (let i = 0; i < collapsibleLength; i++) {
+                    collapsible[i].style.display = 'none';
+                }
+                collapsible[i].style.display = 'flex';
+            }
+        }
+    });
+    
+}
+collapsibleInfo.forEach(element => { //поскольку в нашем макете есть крестик, решил реализовать сворачивание через нажатие на него, а не на текстовую зону
+    element.addEventListener('click', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+}); 
